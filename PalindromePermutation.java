4f04d600af0105d9e0fb08359430df5f6040c91e
skyera <zgliu71@gmail.com>
2017-09-10 09:15:12 -0700

    palindrome permutation

--- PalindromePermutation.java
+++ PalindromePermutation.java
@@ -40,6 +40,58 @@ public class PalindromePermutation {
         return checkMaxOneOld(table);
     }
 
+    // Solution 2
+    boolean isPermutationOfPalindrome2(String phrase) {
+        int countOdd = 0;
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+
+        for (char c: phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            if (x != -1) {
+                table[x]++;
+                if (table[x] % 2 == 1)
+                    countOdd++;
+                else
+                    countOdd--;
+            }
+        }
+
+        return countOdd <= 1;
+    }
+
+    // Solution 3
+    boolean isPermutationOfPalindrome3(String phrase) {
+        int bitVector = createBitVector(phrase);
+        return bitVector == 0 || checkExactlyOneBitSet(bitVector);
+    }
+
+    int createBitVector(String phrase) {
+        int bitVector = 0;
+        for (char c: phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            bitVector = toggle(bitVector, x);
+        }
+        return bitVector;
+    }
+
+    int toggle(int bitVector, int index) {
+        if (index < 0) return bitVector;
+
+        int mask = 1 << index;
+        if ((bitVector & mask) == 0) { 
+            // bit is 0, change it to 1
+            bitVector |= mask;
+        } else {
+            // bit is 1, change it to 0
+            bitVector &= ~mask;
+        }
+        return bitVector;
+    }
+
+    boolean checkExactlyOneBitSet(int bitVector) {
+        return (bitVector & (bitVector - 1)) == 0;
+    }
+
     public static void main(String[] args) {
     
     }
