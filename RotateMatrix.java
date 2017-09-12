6f840300a1dafe0091621d582e58e9c7d39e48fd
skyera <zgliu71@gmail.com>
2017-09-12 00:22:22 -0700

    Rotate matrix

---
+++ RotateMatrix.java
@@ -0,0 +1,27 @@
+// 1.7 Rotate matrix
+// 9/12/2017
+public class RotateMatrix {
+    public static boolean rotate(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length) return false;
+        int n = matrix.length;
+
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i];
+                matrix[first][i] = matrix[last-offset][first];
+                matrix[last-offset][first] = matrix[last][last-offset];
+                matrix[last][last-offset] = matrix[i][last];
+                matrix[i][last] = top;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+    
+    }
+}
