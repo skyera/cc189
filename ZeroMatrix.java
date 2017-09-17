c8193dacb1dd9809168d20f1a619de905c0dd4e5
skyera <zgliu71@gmail.com>
2017-09-16 23:49:40 -0700

    Zero matrix

---
+++ ZeroMatrix.java
@@ -0,0 +1,42 @@
+// 1.8 Zero Matrix
+// 9/16/2017
+
+public class ZeroMatrix {
+    public static void setZeros(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < row.length; i++) {
+            if (row[i]) nulliflyRow(matrix, i);
+        }
+
+        for (int j = 0; j < column.length; j++) {
+            if (column[j]) nullifyColumn(matrix, j);
+        }
+    }
+
+    static void nulliflyRow(int[][] matrix, int row) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            matrix[row][j] = 0;
+        }
+    }
+
+    static void nullifyColumn(int[][] matrix, int col) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][col] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+    
+    }
+}
