b14b9520df2d95396c49e97d61a1a0930f87b340
skyera <zgliu71@gmail.com>
2018-07-02 08:08:01 -0700

    permute string

---
+++ PermuteString.java
@@ -0,0 +1,21 @@
+public class PermuteString {
+    public static void permutation(String str) {
+        permutation(str, "");
+    }
+
+    public static void permutation(String str, String prefix) {
+        if (str.length() == 0) {
+            System.out.println(prefix);
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String rem = str.substring(0, i) + str.substring(i + 1);
+                permutation(rem, prefix + str.charAt(i));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        permutation(s);
+    }
+}
