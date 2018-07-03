180cecf9bfa571af97b95943fd13237d62fca5d3
skyera <zgliu71@gmail.com>
2018-07-02 21:35:27 -0700

    fib memo

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -7,6 +7,15 @@ int fib(int n) {
     return fib(n - 1) + fib(n - 2);
 }
 
+int fib2(int n, int memo[]) {
+    if (n <= 0) return 0;
+    else if (n == 1) return 1;
+    else if (memo[n] > 0) return memo[n];
+
+    memo[n] = fib2(n - 1, memo) + fib2(n - 2, memo);
+    return memo[n];
+}
+
 int main() {
     int n = 10;
     int result = fib(n);
